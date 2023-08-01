e37bc5a5eabe9a2728ee6401d8ffcde2567b7578
tyesmith44 <tyesmith44@gmail.com>
2023-07-31 19:42:36 -0700

    challenge 6

---
+++ config.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../')
+
+# OpenWeatherMap API Key
+weather_api_key = "badcda3a4edc0e85b174a5a5e907349b"
+
+# Geoapify API Key
+geoapify_key = "5ada198c79704b0984d46b58e1367602"
